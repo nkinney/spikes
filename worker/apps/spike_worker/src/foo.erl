6f72d7b8fca86ec18e69439d69d1b565db212967
Nick Kinney <nkinney@virtualhold.com>
2015-04-23 15:51:44 -0400

    updated spike worker to run a sample app to add 2 number together.

--- worker/apps/spike_worker/src/foo.erl
+++ worker/apps/spike_worker/src/foo.erl
@@ -8,12 +8,15 @@
 start_link() ->
     simple_worker:start_link(?MODULE).
 
-handle_command_msg(Command) ->
-    io:format(" [x] Msg Recieved: ~p~n", [Command]),
-    Dots = length([C || C <- binary_to_list(Command), C == $.]),
-    receive
-    after
-        Dots * 1000 -> ok
-    end,
-    io:format(" [x] Done~n"),
-    ok.
+handle_command_msg(Body) ->
+    lager:info(" [x] Msg Recieved: ~p", [Body]),
+    {struct,PropList} = mochijson2:decode(Body),
+    NumbersToAdd = proplists:get_value(<<"params">>,PropList),
+    Sum = ensure_binary(mochijson2:encode([ok,lists:sum(NumbersToAdd)])),
+    lager:info(" [x] Msg Reply: ~p", [Sum]),
+    Sum.
+
+ensure_binary(B) when is_binary(B) ->
+	B;
+ensure_binary(B) when is_list(B) ->
+	list_to_binary(B).
--- worker/apps/spike_worker/src/simple_worker.erl
+++ worker/apps/spike_worker/src/simple_worker.erl
@@ -7,17 +7,17 @@
 -export([start_link/1]).
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
 
--record (state, {domain_module}).
+-record (state, {domain_module,channel}).
 
 start_link(DomainModule) ->
     gen_server:start_link({local, DomainModule}, ?MODULE, [DomainModule], []).
 
 init([DomainModule]) ->
-    Queue = <<"hello">>,
+    Queue = <<"add_numbers">>,
     {_Connection, Channel} = create_queue("localhost", Queue),
     io:format(" [*] Waiting for messages. To exit press CTRL+C~n"),
     amqp_channel:subscribe(Channel, #'basic.consume'{queue = Queue, no_ack = true}, self()),
-    {ok, #state{domain_module=DomainModule}}.
+    {ok, #state{domain_module=DomainModule,channel=Channel}}.
 
 handle_call(Msg, From, State) ->
     lager:info("unexpected handle_call(~p, ~p, ~p) -> {reply, ok, ~p}",[Msg,From,State,State]),
@@ -30,10 +30,13 @@ handle_cast(Msg, State) ->
 handle_info(#'basic.consume_ok'{}, State) ->
     lager:info("basic.consume_ok"),
     {noreply, State};
-handle_info({#'basic.deliver'{}, #amqp_msg{payload=Body}}, State) ->
+handle_info({#'basic.deliver'{}, #amqp_msg{payload=Body,props=Props}}, State) ->
+    ReplyToQueue = Props#'P_basic'.reply_to,
     lager:info("basic.deliver  Body: ~p", [Body]),
     Module = State#state.domain_module,
-    Module:handle_command_msg(Body),
+    Sum = Module:handle_command_msg(Body),
+    % amqp_channel:call(State#state.channel, #'queue.declare'{queue = ReplyToQueue}),
+    amqp_channel:cast(State#state.channel, #'basic.publish'{routing_key = ReplyToQueue},#'amqp_msg'{payload=Sum}),
     {noreply, State};
 handle_info(Info, State) ->
     lager:warning("others  Info: ~p", [Info]),
